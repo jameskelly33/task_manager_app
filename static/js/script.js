6a70f92e929fc85845741edd29e9e126ff1b7470
jameskelly33 <jameskelly33@gmail.com>
2021-09-27 12:44:06 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
   $(".sidenav").sidenav({edge: "right"});
   $(".collapsible").collapsible();
   $(".tooltipped").tooltip();
+  $('select').formSelect()
   $(".datepicker").datepicker({
       format: "dd mmmm, yyyy",
       yearRange: 3,
@@ -10,5 +11,6 @@ $(document).ready(function () {
       i18n: {
           done: "Select"
       }
+
   });
 });
